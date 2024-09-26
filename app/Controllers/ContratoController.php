2dda6c76b1bc8ac0add6d898dec6b1f6dbab3e0f
Rawdney Mendes <mendesrawdney@gmail.com>
2024-09-26 14:25:15 -0300

    added: added swagguer to aplication

--- app/Controllers/ContratoController.php
+++ app/Controllers/ContratoController.php
@@ -4,8 +4,17 @@ namespace App\Controllers;
 
 use App\UseCases\ListarContratosUseCase;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
 
-class ContratoController
+/**
+ * @OA\Info(
+ *     title="API de Contratos",
+ *     version="1.0",
+ *     description="API para gerenciamento de contratos."
+ * )
+ * @OA\Server(url="http://localhost:8000")
+ */
+class ContratoController extends BaseController
 {
     private $listarContratosUseCase;
 
@@ -14,6 +23,15 @@ class ContratoController
         $this->listarContratosUseCase = $listarContratosUseCase;
     }
 
+    /**
+     * @OA\Get(
+     *     path="/contratos",
+     *     summary="Listar contratos",
+     *     description="Retorna uma lista de contratos.",
+     *     @OA\Response(response="200", description="Lista de contratos retornada com sucesso"),
+     *     @OA\Response(response="404", description="Contratos não encontrados"),
+     * )
+     */
     public function index(): JsonResponse
     {
         $contratos = $this->listarContratosUseCase->execute();
